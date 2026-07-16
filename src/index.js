37f5bfde5ec2af2db5ca20ca20bc04c3931ac41d
mannan654 <0808ds221068.ies@ipsacademy.org>
2026-07-16 16:36:03 +0530

    done with basic file and folder setup

---
+++ src/index.js
@@ -0,0 +1,23 @@
+
+import dotenv from "dotenv"
+import mongoose from "mongoose"
+import { DB_NAME } from "./constants.js"
+import connectDB from "./db/index.js"
+import { app } from "./app.js"
+
+
+dotenv.config({
+    path: './.env'
+})
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`listening to port ${PORT}`);
+        
+    })
+})
+.catch((error) => {
+    console.log("MongoDB connection failed !!",error);
+    
+})
